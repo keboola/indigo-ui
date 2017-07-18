cf7a01db339d7f91561923cd887506b30f7aaee6
Jan Michek <jan.michek@designeo.cz>
2017-07-18 18:02:12 +0200

    table example added

--- src/stories/Table/index.js
+++ src/stories/Table/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import { Table } from 'react-bootstrap';
+import {Table, Button} from 'react-bootstrap';
 
 
 storiesOf('Table')
@@ -41,5 +41,67 @@ storiesOf('Table')
         {
             inline: true,
         }
-    )
+    ).addWithInfo(
+    'with actions',
+    'Desc',
+    () => (
+        <Table striped bordered condensed hover>
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>
+                    <Button bsStyle="link">
+                        <i className="fa kbc-icon-cup"></i>
+                    </Button>
+                    <Button bsStyle="link">
+                        <i className="fa fa-fw fa-play"></i>
+                    </Button>
+                </td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>Thornton</td>
+                <td>
+                    <Button bsStyle="link">
+                        <i className="fa kbc-icon-cup"></i>
+                    </Button>
+                    <Button bsStyle="link">
+                        <i className="fa fa-fw fa-play"></i>
+                    </Button>
+                </td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td colSpan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                <td>
+                    <Button bsStyle="link">
+                        <i className="fa kbc-icon-cup"></i>
+                    </Button>
+                    <Button bsStyle="link">
+                        <i className="fa fa-fw fa-play"></i>
+                    </Button>
+                </td>
+            </tr>
+            </tbody>
+        </Table>
+    ),
+    {
+        inline: true,
+    }
+)
 ;
