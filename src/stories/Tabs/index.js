84123b8864adf3980800fe2eb655fdaf8348fd57
Jan Michek <jan.michek@designeo.cz>
2017-07-18 18:31:28 +0200

    tabs  example added

--- src/stories/Tabs/index.js
+++ src/stories/Tabs/index.js
@@ -12,7 +12,7 @@ storiesOf('Tabs')
         <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
             <Tab eventKey={1} title="Tab 1">Tab 1 content</Tab>
             <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
-            <Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</Tab>
+            <Tab eventKey={3} title="Tab 3">Tab 3 content</Tab>
         </Tabs>
     ),
       {
