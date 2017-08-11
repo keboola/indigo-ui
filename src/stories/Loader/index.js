c3bfaa3a0fd07b927b3540f1a6b7def9a8da2141
Jan Michek <jan.michek@designeo.cz>
2017-08-11 14:06:32 +0200

    cool new loading animation

--- src/stories/Loader/index.js
+++ src/stories/Loader/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Loader } from 'kbc-react-components';
+import { Image } from 'react-bootstrap';
+import BubbleLoader from '../../static/bubble-loader.svg';
+
 
 storiesOf('Loader')
   .addWithInfo(
@@ -8,6 +11,20 @@ storiesOf('Loader')
     '',
     () => (
       <Loader classNamee="fa-fw" />
+    ),
+    {
+        inline: true,
+    }
+  )
+    .addWithInfo(
+    '🆕  Bubble Loader',
+    '',
+    () => (
+      <div className="bubble-loader">
+        <Image src={BubbleLoader}/>
+          <p>Loading</p>
+      </div>
+
     ),
     {
         inline: true,
