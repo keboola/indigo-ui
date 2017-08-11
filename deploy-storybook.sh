c83dc8f8bc93dc2b4dca608a72d33b325dda0137
Vladimír Kriška <vlado@keboola.com>
2017-08-11 15:54:22 +0200

    Fix exclude parameter when copying index.html

--- deploy-storybook.sh
+++ deploy-storybook.sh
@@ -7,4 +7,4 @@ docker-compose run --rm \
 && docker-compose run --rm \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  aws s3 cp build s3://indigo.keboola.com --recursive --include "index.html" --acl "public-read" --cache-control "no-cache"
+  aws s3 cp build s3://indigo.keboola.com --recursive --exclude "*" --include "index.html" --acl "public-read" --cache-control "no-cache"
