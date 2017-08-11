de2c7df9983fb7b60898a00fd264339c6a5ca60d
Vladimír Kriška <vlado@keboola.com>
2017-08-11 15:40:11 +0200

    Copy index.html separately with cache set to no-cache

--- deploy-storybook.sh
+++ deploy-storybook.sh
@@ -3,4 +3,8 @@
 docker-compose run --rm \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  aws s3 cp build s3://indigo.keboola.com --recursive --acl "public-read"
+  aws s3 cp build s3://indigo.keboola.com --recursive --exclude "index.html" --acl "public-read" \
+&& docker-compose run --rm \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  aws s3 cp build s3://indigo.keboola.com --recursive --include "index.html" --acl "public-read" --cache-control "no-cache"
