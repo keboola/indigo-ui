d2a359750944ba78e9e99a56aa3724cd59b5135a
Jan Michek <jan.michek@designeo.cz>
2017-10-17 13:12:53 +0200

    new buttons demo

--- src/stories/Button/index.js
+++ src/stories/Button/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {Button} from 'react-bootstrap';
+import Icon from "../../static/common/Icon";
 
 storiesOf('Button')
     .addWithInfo(
@@ -135,6 +136,349 @@ storiesOf('Button')
             <Button bsStyle="link">
                 <span className="fa fa-pencil"></span>
             </Button>
+        ),
+        {
+            inline: true,
+        }
+    ).addWithInfo(
+        'Button Festival',
+        'Desc',
+        () => (
+            <div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Status
+                    </div>
+                    <div className="col-md-3">
+                        Icon 20x20 + Text
+                    </div>
+                    <div className="col-md-3">
+                        Text
+                    </div>
+                    <div className="col-md-3">
+                        Icon
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                            Normal
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="success">
+                            <i className="kbc-icon-plus"/>
+                            <span className="btn-label">
+                                New Extractor
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="success">
+                            <span className="btn-label">
+                                New Extractor
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="success">
+                            <i className="kbc-icon-plus"/>
+                        </Button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                            Active
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="success" className="active">
+                            <i className="kbc-icon-plus"/>
+                            <span className="btn-label">
+                                New Extractor
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="success" className="active">
+                            <span className="btn-label">
+                                New Extractor
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="success" className="active">
+                            <i className="kbc-icon-plus"/>
+                        </Button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                            Disabled
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="success" disabled>
+                            <i className="kbc-icon-plus"/>
+                            <span className="btn-label">
+                                New Extractor
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="success" disabled>
+                            <span className="btn-label">
+                                New Extractor
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="success" disabled>
+                            <i className="kbc-icon-plus"/>
+                        </Button>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-3">
+                        <br/><br/><br/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Normal
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary">
+                            <i className="kbc-icon-plus"/>
+                            <span className="btn-label">
+                                Add Input
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary">
+                            <span className="btn-label">
+                                Add Input
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary">
+                            <i className="kbc-icon-plus"/>
+                        </Button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Active
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary" className="active">
+                            <i className="kbc-icon-plus"/>
+                            <span className="btn-label">
+                                Add Input
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary" className="active">
+                            <span className="btn-label">
+                                Add Input
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary" className="active">
+                            <i className="kbc-icon-plus"/>
+                        </Button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Disabled
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary" disabled>
+                            <i className="kbc-icon-plus"/>
+                            <span className="btn-label">
+                                Add Input
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary" disabled>
+                            <span className="btn-label">
+                                Add Input
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary" disabled>
+                            <i className="kbc-icon-plus"/>
+                        </Button>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-3">
+                        <br/><br/><br/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Normal
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="danger">
+                            <i className="fa fa-trash"/>
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="danger">
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="danger">
+                            <i className="fa fa-trash"/>
+                        </Button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Active
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="danger" className="active">
+                            <i className="fa fa-trash"/>
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="danger" className="active">
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="danger" className="active">
+                            <i className="fa fa-trash"/>
+                        </Button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Disabled
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary" disabled>
+                            <i className="fa fa-trash"/>
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary" disabled>
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="primary" disabled>
+                            <i className="fa fa-trash"/>
+                        </Button>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-3">
+                        <br/><br/><br/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Normal
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="link">
+                            <i className="fa fa-trash"/>
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="link">
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="link">
+                            <i className="fa fa-trash"/>
+                        </Button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Active
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="link" className="active">
+                            <i className="fa fa-trash"/>
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="link" className="active">
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="link" className="active">
+                            <i className="fa fa-trash"/>
+                        </Button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3">
+                        Disabled
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="link" disabled>
+                            <i className="fa fa-trash"/>
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="link" disabled>
+                            <span className="btn-label">
+                                Delete Project
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="col-md-3">
+                        <Button bsStyle="link" disabled>
+                            <i className="fa fa-trash"/>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+
         ),
         {
             inline: true,
