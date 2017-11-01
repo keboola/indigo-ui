3eaf104bf82a39d1c0861141dafe03be4a68f352
Jan Michek <jan.michek@designeo.cz>
2017-11-01 15:01:27 +0100

    select styles

--- src/stories/Select/index.js
+++ src/stories/Select/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Select from 'react-select';
+import Icon from "../../static/common/Icon";
+
 
 
 var options = [
     { value: 'one', label: 'One' },
-    { value: 'two', label: 'Two' }
+    { value: 'one and half', label: 'One and Half' },
+    { value: 'three', label: 'Three' },
+    { value: 'two', label: '#89959f' },
+    { value: 'two', label: '#373b46' },
+    { value: 'two', label: '#22262f' },
+    { value: 'two', label: '#1e2129' },
+    { value: 'two', label: '#89959f' },
+    { value: 'two', label: '#373b46' },
+    { value: 'two', label: '#22262f' },
+    { value: 'two', label: '#1e2129' },
+    { value: 'two', label: '#89959f' },
+    { value: 'two', label: '#373b46' }
 ];
 
 function logChange(val) {
     console.log("Selected: " + JSON.stringify(val));
 }
+
 
 storiesOf('Select')
   .addWithInfo(
@@ -19,9 +33,12 @@ storiesOf('Select')
     () => (
         <Select
             name="form-field-name"
-            value="one"
+            value="three"
             options={options}
             onChange={logChange}
+            placeholder="Select placeholder..."
+            arrowRenderer={function() {return (<Icon iconClass="arrow-down">+</Icon>)}}
+            clearRenderer={function() {return (<Icon iconClass="times">+</Icon>)}}
         />
     ),
     {
