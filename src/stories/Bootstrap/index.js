a8a5beb87736b18365c53c5c6e5262a39d4bb341
Vladimír Kriška <vlado@keboola.com>
2017-12-11 21:46:00 +0100

    Fix DropdownButton warning; Remove styles keeping it open

--- src/stories/Bootstrap/index.js
+++ src/stories/Bootstrap/index.js
@@ -105,18 +105,18 @@ storiesOf('Bootstrap')
     'Desc',
     () => (
       <div className="container bs-docs-container">
-                <div className="bs-docs-section">
-                    <h1>DropdownButton</h1>
-                    <div className="bs-example">
-                        <DropdownButton title="DropdownButton">
-                            <MenuItem eventKey="1">Action</MenuItem>
-                            <MenuItem eventKey="2">Another action</MenuItem>
-                            <MenuItem eventKey="3" active>Active Item</MenuItem>
-                            <MenuItem divider/>
-                            <MenuItem eventKey="4">Separated link</MenuItem>
-                        </DropdownButton>
-                    </div>
-                </div>
+        <div className="bs-docs-section">
+            <h1>DropdownButton</h1>
+            <div className="bs-example">
+                <DropdownButton title="DropdownButton" id="bootstrap-showcase-dropdown-button">
+                    <MenuItem eventKey="1">Action</MenuItem>
+                    <MenuItem eventKey="2">Another action</MenuItem>
+                    <MenuItem eventKey="3" active>Active Item</MenuItem>
+                    <MenuItem divider/>
+                    <MenuItem eventKey="4">Separated link</MenuItem>
+                </DropdownButton>
+            </div>
+        </div>
       </div>
     ))
   .addWithInfo(
