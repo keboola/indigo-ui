022c67c23e4bdada98629147da3453b83e035b9b
Vladimír Kriška <vlado@keboola.com>
2017-12-14 22:43:08 +0100

    Use small hack to load all components (to lint them)
    [ci skip]

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,9 @@ import { configure, setAddon, addDecorator } from '@storybook/react';
 import infoAddon from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
 
+// Load all components, so we can lint them. Do not remove.
+import components from '../src/indigo/components';
+
 function loadStories() {
   require('../src/stories/Bootstrap');
   require('../src/stories/Sample');
