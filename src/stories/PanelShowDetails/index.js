2552bbd049a1b58f5245e69b4fb5fb6f2d04c2b1
Vladimír Kriška <vlado@keboola.com>
2018-03-28 19:40:36 +0200

    Update Panel/panel-show-details story

--- src/stories/PanelShowDetails/index.js
+++ src/stories/PanelShowDetails/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import {storiesOf} from '@storybook/react';
-import {
-    Panel
-} from 'react-bootstrap';
+import { storiesOf } from '@storybook/react';
+import { Panel } from 'react-bootstrap';
+import { withInfo } from '@storybook/addon-info';
 
-
-
-storiesOf('PanelShowDetails', module)
-    .add('PanelShowDetails', () => (
-        <Panel
-            collapsible
-            expanded={false}
-            header="Show details"
-            className="panel-show-details">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life
-            accusamus terry richardson ad squid. Nihil anim keffiyeh
-            helvetica, craft beer labore wes anderson cred nesciunt sapiente
-            ea proident.
-        </Panel>
-        )
-    );
+storiesOf('[CSS] Panel', module).add(
+  'panel-show-details',
+  withInfo({
+    text: `
+        Add *className="panel-show-details"* to use Panel as "Show more/Show details" component.
+        `,
+    inline: true,
+  })(() => {
+    return <Panel
+      collapsible
+      header="Show details"
+      className="panel-show-details"
+    >
+      Anim pariatur cliche reprehenderit, enim eiusmod high life
+      accusamus terry richardson ad squid. Nihil anim keffiyeh
+      helvetica, craft beer labore wes anderson cred nesciunt sapiente
+      ea proident.
+    </Panel>;
+  })
+);
