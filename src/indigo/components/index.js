08249136e711e38ab58558b5646ec0848699f9a4
Vladimír Kriška <vlado@keboola.com>
2018-04-10 15:41:48 +0200

    Add missing export; Sort exports alphapetically

--- src/indigo/components/index.js
+++ src/indigo/components/index.js
@@ -5,13 +5,15 @@ import Protected from './Protected';
 import Tree from './Tree';
 import NewLineToBr from './NewLineToBr';
 import ExternalLink from './ExternalLink';
+import PanelWithDetails from './PanelWithDetails';
 
 export {
   Check,
+  ExternalLink,
   Loader,
-  RefreshIcon,
-  Protected,
-  Tree,
   NewLineToBr,
-  ExternalLink
+  PanelWithDetails,
+  Protected,
+  RefreshIcon,
+  Tree,
 };
