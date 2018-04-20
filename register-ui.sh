97511b9ddc9df96dfc22bcc56e6c5e7cfc575db7
Vladimír Kriška <vlado@keboola.com>
2018-04-20 10:56:38 +0200

    Remove code depreacted by indigo-ui-kbc

--- register-ui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo $INDIGO_UI_REVISION
-export BODY="{\"manifestUrl\":\"https://assets-cdn.keboola.com/indigo-ui/$INDIGO_UI_REVISION/manifest.json\",\"activate\":true}"
-curl -X POST \
-  -H X-KBC-ManageApiToken:$KBC_MANAGE_API_TOKEN \
-  -H "Content-Type:application/json" \
-  -d $BODY \
-  https://connection.keboola.com/manage/ui-apps
