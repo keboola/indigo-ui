2f0891af33fceee78c70052465e28b338cf712ec
Vladimír Kriška <vlado@keboola.com>
2018-05-03 21:27:07 +0200

    Rename AlertBlock prop (to fix conflict with style prop)

--- src/indigo/components/AlertBlock.jsx
+++ src/indigo/components/AlertBlock.jsx
@@ -4,14 +4,14 @@ import { Alert } from 'react-bootstrap';
 export default React.createClass({
   propTypes: {
     title: PropTypes.string.isRequired,
-    style: PropTypes.oneOf(['warning', 'danger']).isRequired,
+    type: PropTypes.oneOf(['warning', 'danger']).isRequired,
     children: PropTypes.any.isRequired
   },
 
   render() {
-    const { style, title, children } = this.props;
+    const { type, title, children } = this.props;
     return (
-      <Alert className="alert-block" bsStyle={style}>
+      <Alert className="alert-block" bsStyle={type}>
         <h3 className="alert-block-title">{title}</h3>
         <div className="alert-block-body">
           {children}
--- src/indigo/components/AlertBlock.test.js
+++ src/indigo/components/AlertBlock.test.js
@@ -5,10 +5,10 @@ import AlertBlock from './AlertBlock';
 
 describe('<AlertBlock />', () => {
   it('AlertBlock - warning', () => {
-    snapshot(<AlertBlock title="This is warning" style="warning">Warning text</AlertBlock>);
+    snapshot(<AlertBlock title="This is warning" type="warning">Warning text</AlertBlock>);
   });
 
   it('AlertBlock - danger', () => {
-    snapshot(<AlertBlock title="This is danger" style="danger">Danger text</AlertBlock>);
+    snapshot(<AlertBlock title="This is danger" type="danger">Danger text</AlertBlock>);
   });
 });
