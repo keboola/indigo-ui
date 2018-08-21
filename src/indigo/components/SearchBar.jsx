a45414243fe272a922f84746d8e7b037013e4824
janmichek <jan@keboola.com>
2018-08-21 17:50:31 +0200

    refactor function to stateless type

--- src/indigo/components/SearchBar.jsx
+++ src/indigo/components/SearchBar.jsx
@@ -1,52 +1,35 @@
 import React from 'react';
-import { ButtonToolbar, FormControl } from 'react-bootstrap';
-import { Icon } from '../../indigo/components';
+import {ButtonToolbar, FormControl} from 'react-bootstrap';
+import Icon from './icons'
+import PropTypes from 'prop-types';
 
-export default React.createClass({
 
-  propTypes: {
-    query: React.PropTypes.string.isRequired,
-    onChange: React.PropTypes.func,
-    onSubmit: React.PropTypes.func,
-    className: React.PropTypes.string,
-    placeholder: React.PropTypes.string,
-    actions: React.PropTypes.object
-  },
-
-  getInitialState() {
-    return {
-      query: this.props.query
-    };
-  },
-
-  getDefaultProps() {
-    return {
-      onChange: () => {},
-      onSubmit: (query) => query,
-      placeholder: 'Search'
-    };
-  },
-
-  onChange(event) {
-    this.setState({
-      query: event.target.value
-    });
-    this.props.onChange(event.target.value);
-  },
-
-  onSubmit(event) {
+class SearchBar extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {query: this.props.query};
+    this.changeQuery = this.changeQuery.bind(this)
+    this.submitQuery = this.submitQuery.bind(this)
+  }
+  
+  changeQuery(event) {
+    this.setState({query: event.target.value})
+  }
+  
+  submitQuery(event) {
     event.preventDefault();
     this.props.onSubmit(this.state.query);
-  },
-
+  }
+  
   render() {
     return (
-      <form className="searchbar" onSubmit={this.onSubmit}>
+      <form className="searchbar" onSubmit={(event) => this.submitQuery(event)}>
         <Icon.Search className="searchbar-icon icon-size-20"/>
         <FormControl
           value={this.state.query}
           placeholder={this.props.placeholder}
-          onChange={this.onChange}
+          onChange={(event) => this.changeQuery(event)}
           className="searchbar-input"
           ref="searchInput"
           type="text"
@@ -60,5 +43,23 @@ export default React.createClass({
       </form>
     );
   }
+}
 
-});
+SearchBar.defaultProps = {
+  onChange: () => {
+  },
+  onSubmit: (query) => query,
+  placeholder: 'Search'
+};
+
+
+SearchBar.propTypes = {
+  query: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+  actions: PropTypes.object
+};
+
+export default SearchBar;
--- src/indigo/components/index.js
+++ src/indigo/components/index.js
@@ -4,6 +4,7 @@ import RefreshIcon from './RefreshIcon';
 import Protected from './Protected';
 import Tree from './Tree';
 import NewLineToBr from './NewLineToBr';
+import SearchBar from './SearchBar';
 import ExternalLink from './ExternalLink';
 import PanelWithDetails from './PanelWithDetails';
 import AlertBlock from './AlertBlock';
@@ -20,4 +21,5 @@ export {
   Tree,
   AlertBlock,
   Icon,
+  SearchBar
 };
