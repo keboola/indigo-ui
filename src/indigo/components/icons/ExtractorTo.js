264745d25d619d4e8e49fe64bbe1c5f9e63c044f
Vladimír Kriška <vlado@keboola.com>
2018-09-26 14:49:09 +0200

    Regenerate icons

--- src/indigo/components/icons/ExtractorTo.js
+++ src/indigo/components/icons/ExtractorTo.js
@@ -3,23 +3,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const ExtractorTo = ({ className, ...props }) => {
-  return (
-    <svg
-      width="32"
-      height="32"
-      viewBox="0 0 86 50"
-      className={classNames('icon', className)}
-      {...props}
-    >
-      <path
-        d="M44 50H4a4 4 0 0 1-4-4V6a4 4 0 0 1 4-4h14v4H6a2 2 0 0 0-2 2v36a2 2 0 0 0 2 2h36a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2H30V2h14a4 4 0 0 1 4 4v40a4 4 0 0 1-4 4zM30.733 18.584a1.928 1.928 0 0 1 2.703.018c.744.743.753 1.944.02 2.698L25.77 28.97c-.085.177-.198.34-.335.48a2.15 2.15 0 0 1-2.87 0 1.87 1.87 0 0 1-.289-.434L14.544 21.3a1.919 1.919 0 0 1 .02-2.698 1.928 1.928 0 0 1 2.703-.018l4.727 4.717v-21.3C21.994.896 22.89 0 23.999 0c1.108 0 2.005.896 2.005 2.001v21.3l4.73-4.717zM64 12l22 14-22 14V12z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+const ExtractorTo = ({ className, ...props }) => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 86 50"
+    className={classNames('icon', className)}
+    {...props}
+  >
+    <path
+      d="M44 50H4a4 4 0 0 1-4-4V6a4 4 0 0 1 4-4h14v4H6a2 2 0 0 0-2 2v36a2 2 0 0 0 2 2h36a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2H30V2h14a4 4 0 0 1 4 4v40a4 4 0 0 1-4 4zM30.733 18.584a1.928 1.928 0 0 1 2.703.018c.744.743.753 1.944.02 2.698L25.77 28.97c-.085.177-.198.34-.335.48a2.15 2.15 0 0 1-2.87 0 1.87 1.87 0 0 1-.289-.434L14.544 21.3a1.919 1.919 0 0 1 .02-2.698 1.928 1.928 0 0 1 2.703-.018l4.727 4.717v-21.3C21.994.896 22.89 0 23.999 0c1.108 0 2.005.896 2.005 2.001v21.3l4.73-4.717zM64 12l22 14-22 14V12z"
+      fill="currentColor"
+      fillRule="evenodd"
+    />
+  </svg>
+);
 
 ExtractorTo.propTypes = {
   className: PropTypes.string,
--- src/indigo/components/icons/SwitchOff.js
+++ src/indigo/components/icons/SwitchOff.js
@@ -3,23 +3,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const SwitchOff = ({ className, ...props }) => {
-  return (
-    <svg
-      width="32"
-      height="32"
-      viewBox="0 0 32 20"
-      className={classNames('icon', className)}
-      {...props}
-    >
-      <path
-        d="M2 10c0 4.42 3.579 8 7.993 8h12.014C26.415 18 30 14.415 30 10c0-4.42-3.579-8-7.993-8H9.993C5.585 2 2 5.585 2 10zm-2 0C0 4.477 4.483 0 9.993 0h12.014C27.526 0 32 4.476 32 10c0 5.523-4.483 10-9.993 10H9.993C4.474 20 0 15.524 0 10zm10 6a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
-        fillRule="nonzero"
-        fill="currentColor"
-      />
-    </svg>
-  );
-};
+const SwitchOff = ({ className, ...props }) => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 20"
+    className={classNames('icon', className)}
+    {...props}
+  >
+    <path
+      d="M2 10c0 4.42 3.579 8 7.993 8h12.014C26.415 18 30 14.415 30 10c0-4.42-3.579-8-7.993-8H9.993C5.585 2 2 5.585 2 10zm-2 0C0 4.477 4.483 0 9.993 0h12.014C27.526 0 32 4.476 32 10c0 5.523-4.483 10-9.993 10H9.993C4.474 20 0 15.524 0 10zm10 6a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
+      fillRule="nonzero"
+      fill="currentColor"
+    />
+  </svg>
+);
 
 SwitchOff.propTypes = {
   className: PropTypes.string,
