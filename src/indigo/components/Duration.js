433d85c075cefb2c968d8565d76260b0b94292e0
janmichek <jan@keboola.com>
2018-11-20 16:43:30 +0100

    adds duration demo

--- src/indigo/components/Duration.js
+++ src/indigo/components/Duration.js
@@ -1,8 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import moment from 'moment';
+/* eslint-disable arrow-body-style */
+
+export const durationFrom = (started, finished) => {
+  return (new Date(finished).getTime() - new Date(started).getTime()) / 1000;
+};
+
+export const timeInWords = (duration, round) => {
+  const days = Math.floor(duration / 86400);
+  const hours = Math.floor((duration % 86400) / 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = duration % 60;
+  if (days > 0) {
+    return 'more than 24 hrs';
+  }
+  if (duration === 0) {
+    return '0 sec';
+  }
+  const result = [];
+  if (hours === 1) {
+    result.push(`${hours} hr`);
+  }
+  if (hours > 1) {
+    result.push(`${hours} hrs`);
+  }
+  if (minutes > 0) {
+    result.push(`${minutes} min`);
+  }
+  if (seconds > 0 && (!round || hours === 0)) {
+    result.push(`${seconds} sec`);
+  }
+  if (result.length > 0) {
+    return result.join(' ');
+  }
+  return '';
+};
 
 class Duration extends React.Component {
+  constructor(props) {
+    super(props);
+    this.tick = this.tick.bind(this);
+    this.state = {
+      endTime: new Date().toString(),
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    this.setState({
+      endTime: new Date().toString(),
+    });
+  }
+
   render() {
     if (!this.props.startTime) {
       return null;
@@ -10,17 +66,11 @@ class Duration extends React.Component {
 
     if (!this.props.endTime) {
       return (
-        <span>
-          aaa
-          {moment(this.props.startTime - this.props.endTime).fromNow(true)}
-        </span>
+        <span>{timeInWords(durationFrom(this.props.startTime, this.state.endTime), true)}</span>
       );
-      // <DynamicDuration startTime={this.props.startTime} />;
     }
 
-    return null;
-
-    // return <StaticDuration startTime={this.props.startTime} endTime={this.props.endTime}/>;
+    return <span>{timeInWords(durationFrom(this.props.startTime, this.props.endTime), true)}</span>;
   }
 }
 
--- src/stories/Duration/index.js
+++ src/stories/Duration/index.js
@@ -4,6 +4,9 @@ import { withInfo } from '@storybook/addon-info';
 
 import Duration from '../../indigo/components/Duration';
 
+const date = new Date();
+const date2 = new Date(date.getTime() + 123456)
+
 storiesOf('Duration', module).add(
   'Duration',
   withInfo({
@@ -12,6 +15,17 @@ storiesOf('Duration', module).add(
       `,
     inline: true,
   })(() => {
-    return <Duration endTime={56500000} startTime={56545465} />;
+    return <Duration startTime={date.toString()} endTime={date2.toString()}  />;
+  })
+)
+  .add(
+  'Duration Dynamix',
+  withInfo({
+    text: `
+      Show if something is enabled or not.
+      `,
+    inline: true,
+  })(() => {
+    return <Duration startTime={date.toString()} />;
   })
 );
