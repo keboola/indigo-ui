98fe93215d5c7b86d67d34440e1cd207c8938375
janmichek <jan@keboola.com>
2018-11-21 16:03:47 +0100

    typo

--- src/indigo/components/Finished.test.js
+++ src/indigo/components/Finished.test.js
@@ -9,7 +9,7 @@ const _Date = Date;
 global.Date = jest.fn(() => DATE_TO_USE);
 global.Date.UTC = _Date.UTC;
 global.Date.parse = _Date.parse;
-global.Date.now = _Date.n
+global.Date.now = _Date.now;
 const timeMock = moment(DATE_TO_USE).format('YYYY-MM-DD HH:mm:ss').toString()
 
 describe('<Finished />', () => {
