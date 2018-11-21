6e9e6658ce18a1e769b00c3a6dda0dd906c4c6d0
janmichek <jan@keboola.com>
2018-11-21 11:37:19 +0100

    no time dependent mock

--- src/indigo/components/Finished.test.js
+++ src/indigo/components/Finished.test.js
@@ -4,15 +4,21 @@ import moment from 'moment';
 import { snapshot } from '../../tests';
 import Finished from "./Finished";
 
-const now = moment().format('YYYY-MM-DD HH:mm:ss');
+const DATE_TO_USE = new Date('2016');
+const _Date = Date;
+global.Date = jest.fn(() => DATE_TO_USE);
+global.Date.UTC = _Date.UTC;
+global.Date.parse = _Date.parse;
+global.Date.now = _Date.n
+const timeMock = moment(DATE_TO_USE).format('YYYY-MM-DD HH:mm:ss').toString()
 
 describe('<Finished />', () => {
   it('Basic Init', () => {
-    snapshot(<Finished endTime={now}/>);
+    snapshot(<Finished endTime={timeMock}/>);
   });
 
   it('Finished with icon', () => {
-    snapshot(<Finished showIcon endTime={now}/>);
+    snapshot(<Finished showIcon endTime={timeMock}/>);
   });
 
   it('No datetime provided', () => {
