c50e135e32621fd41ab118208b1a54eff1d4d61c
janmichek <jan@keboola.com>
2018-11-21 13:21:08 +0100

    imporves demo & description

--- src/stories/Duration/index.js
+++ src/stories/Duration/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
+import {storiesOf} from '@storybook/react';
+import {withInfo} from '@storybook/addon-info';
 
 import Duration from '../../indigo/components/Duration';
 
@@ -8,13 +8,14 @@ const now = new Date();
 const demoDatetime1 = now.toString();
 const demoDatetime2 = new Date(now.getTime() + 123456).toString();
 const demoDatetime3 = new Date(now.getTime() + 123456789).toString();
+const demoSecondCount = 1586.36943;
 
 storiesOf('Duration', module)
   .add(
     'Duration',
     withInfo({
       text: `
-      Static duration component for start and end time set. Accepts props in datetime string format.
+      Static duration component. Renders duration from start till end time given. Accepts props in datetime string format.
       `,
       inline: true,
     })(() => {
@@ -22,21 +23,10 @@ storiesOf('Duration', module)
     })
   )
   .add(
-    'Duration > 24h',
-    withInfo({
-      text: `
-      Static duration component for start and end time set. Accepts props in datetime string format.
-      `,
-      inline: true,
-    })(() => {
-      return <Duration startTime={demoDatetime1} endTime={demoDatetime3}/>;
-    })
-  )
-  .add(
     'Duration Dynamic',
     withInfo({
       text: `
-      Active duration counter. Counts from start time given till 24h. Accepts props in datetime string format.
+      Active duration counter. Renders duration from start time till now.
       `,
       inline: true,
     })(() => {
@@ -44,10 +34,34 @@ storiesOf('Duration', module)
     })
   )
   .add(
+    'Duration only',
+    withInfo({
+      text: `
+      Only formats and rounds for seconds count given. Accepts props in number format - number of seconds.
+      `,
+      inline: true,
+    })(() => {
+      return <Duration duration={demoSecondCount}/>;
+    })
+  )
+  
+  .add(
+    'Duration > 24h',
+    withInfo({
+      text: `
+      For duration longer than 24h limits the counter.
+      `,
+      inline: true,
+    })(() => {
+      return <Duration startTime={demoDatetime1} endTime={demoDatetime3}/>;
+    })
+  )
+  
+  .add(
     'Duration with icon',
     withInfo({
       text: `
-      Active duration counter. Counts from start time given till 24h. Accepts props in datetime string format.
+      Adds icon in front of text.
       `,
       inline: true,
     })(() => {
