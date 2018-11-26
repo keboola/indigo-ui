7bead24060130a332bdefe3d2ca3c94335e785c7
janmichek <jan@keboola.com>
2018-11-26 15:39:27 +0100

    uses moment for test

--- src/indigo/components/Duration.test.js
+++ src/indigo/components/Duration.test.js
@@ -1,29 +1,23 @@
 import React from 'react';
-
 import { snapshot } from '../../tests';
 import Duration from "./Duration";
 import moment from "moment";
 
+const time1 = '2018-11-26T15:11:05+0100';
+const time2 = '2018-11-26T15:11:09+0100';
+const time3 = '2018-11-29T15:11:09+0100';
 
-
-const DATE_NOW = '2018-11-26T15:11:05+0100';
-
-const DATE_FUTURE_1 = '2018-11-26T15:11:09+0100';
-const DATE_FUTURE_2 = '2018-11-29T15:11:09+0100';
-
-
-
-global.Date.now = jest.fn(() => moment().valueOf());
-const DATE_PAST = moment(new Date().getTime() -123456).format('YYYY-MM-DDTHH:MM:ssZZ').toString();
-
+const fakeTimeNow = moment().valueOf()
+global.Date.now = jest.fn(() => fakeTimeNow);
+const timePast = moment(fakeTimeNow - 123456).format('YYYY-MM-DDTHH:MM:ssZZ').toString();
 
 describe('<Duration />', () => {
   it('Duration', () => {
-    snapshot(<Duration startTime={DATE_NOW} endTime={DATE_FUTURE_1}/>);
+    snapshot(<Duration startTime={time1} endTime={time2}/>);
   });
 
   it('Duration Dynamic', () => {
-    snapshot(<Duration startTime={DATE_PAST}/>);
+    snapshot(<Duration startTime={timePast}/>);
   });
 
   it('Duration only', () => {
@@ -31,11 +25,11 @@ describe('<Duration />', () => {
   });
 
   it('Duration > 24h', () => {
-    snapshot(<Duration startTime={DATE_NOW} endTime={DATE_FUTURE_2}/>);
+    snapshot(<Duration startTime={time1} endTime={time3}/>);
   });
 
   it('Duration with icon', () => {
-    snapshot(<Duration startTime={DATE_NOW} endTime={DATE_FUTURE_1} showIcon/>);
+    snapshot(<Duration startTime={time1} endTime={time2} showIcon/>);
   });
   it('Duration - empty state', () => {
     snapshot(<Duration />);
