dbb5a8e109388c34d40417bae6c3a8c379bd6a44
janmichek <jan@keboola.com>
2018-11-26 16:27:52 +0100

    fixes datetime format for test

--- src/indigo/components/Finished.test.js
+++ src/indigo/components/Finished.test.js
@@ -1,23 +1,24 @@
 import React from 'react';
-
+import moment from 'moment';
 import { snapshot } from '../../tests';
 import Finished from "./Finished";
 
-const DATE_NOW = '2016-01-01T00:05:21+0100';
-const DATE_PAST = '2016-01-01T00:00:00+0100';
-
-global.Date.now = jest.fn(() => new Date(DATE_NOW).valueOf());
+const fakeTimeNow = moment('2018-11-26T16:00:00+0100').valueOf()
+global.Date.now = jest.fn(() => fakeTimeNow);
+const timePast = moment(fakeTimeNow - 123456).format('YYYY-MM-DDTHH:mm:ssZZ').toString();
 
 describe('<Finished />', () => {
   it('Basic Init', () => {
-    snapshot(<Finished endTime={DATE_PAST}/>);
+    snapshot(<Finished endTime={timePast}/>);
   });
 
   it('Finished with icon', () => {
-    snapshot(<Finished showIcon endTime={DATE_PAST}/>);
+    snapshot(<Finished showIcon endTime={timePast}/>);
   });
 
   it('No datetime provided', () => {
     snapshot( <Finished/>);
   });
 });
+
+
