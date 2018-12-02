0ca13d6503e11f2f9c90baa524272b85c01af117
Vladimír Kriška <vlado@keboola.com>
2018-12-02 20:33:48 +0100

    Add working example of InlineEditInput

--- src/stories/InlineEditInput/index.js
+++ src/stories/InlineEditInput/index.js
@@ -1,8 +1,65 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import {action} from "@storybook/addon-actions";
 
 import InlineEditInput from '../../indigo/components/InlineEditInput';
+
+export default class InlineEditInputExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialText: '',
+      text: '',
+      isEditing: false,
+      isSaving: false
+    };
+  }
+
+  render() {
+    return (
+      <InlineEditInput
+        isEditing={this.state.isEditing}
+        isValid
+        onEditStart={() => {
+          action('editing started')();
+          this.setState({
+            isEditing: true
+          })
+        }}
+        onEditCancel={() => {
+          action('editing cancelled')();
+          this.setState({
+            isEditing: false,
+            text: this.state.initialText
+          })
+        }}
+        onEditChange={(text) => {
+          this.setState({
+            text
+          })
+        }}
+        onEditSubmit={() => {
+          action('form submitted')();
+          this.setState({
+            isSaving: true,
+          });
+          setTimeout(() => {
+            this.setState({
+              initialText: this.state.text,
+              isEditing: false,
+              isSaving: false,
+            })
+          }, 1000);
+        }}
+        text={this.state.text}
+        isSaving={this.state.isSaving}
+        editTooltip="Click to edit"
+        placeholder="Click to edit"
+      />
+    );
+  }
+}
 
 storiesOf('InlineEditInput', module)
   .add(
@@ -50,7 +107,8 @@ storiesOf('InlineEditInput', module)
         />
       );
     })
-  ).add(
+  )
+  .add(
     'Edit mode - saving',
     withInfo({
       text: `
@@ -71,6 +129,21 @@ storiesOf('InlineEditInput', module)
           editTooltip="Click to edit"
           placeholder="Click to edit"
         />
+      );
+    })
+  )
+  .add(
+    'Working example',
+    withInfo({
+      text: `
+        Full example with actions, etc.
+        `,
+      inline: true
+    })(() => {
+      return (
+        <div>
+          some text <InlineEditInputExample />
+        </div>
       );
     })
   );
