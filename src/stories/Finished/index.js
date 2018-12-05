c92ee724457274a5f129a960d0f2e89ee1ca529d
Vladimír Kriška <vlado@keboola.com>
2018-12-05 21:28:07 +0100

    Remove unnecessary story; Fix code style

--- src/stories/Finished/index.js
+++ src/stories/Finished/index.js
@@ -1,36 +1,32 @@
 import React from 'react';
-import {storiesOf} from '@storybook/react';
-import {withInfo} from '@storybook/addon-info';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 import Finished from '../../indigo/components/Finished';
-import moment from "moment";
-import {boolean} from "@storybook/addon-knobs";
+import moment from 'moment';
+import { boolean } from '@storybook/addon-knobs';
 
 const now = new Date();
-const demoTimestamp = moment(now.getTime() - 123456).format('YYYY-MM-DDTHH:mm:ssZZ').toString();
+const demoTimestamp = moment(now.getTime() - 123456)
+  .format('YYYY-MM-DDTHH:mm:ssZZ')
+  .toString();
 
-storiesOf('Finished', module).add(
-  'Basic Init',
-  withInfo({
-    text: `
-        Shows how much time ago event occured. Shows exact time of event on mouse hover. Accepts prop in datetime string format.
+storiesOf('Finished', module)
+  .add(
+    'Basic Init',
+    withInfo({
+      text: `
+        Shows how much time ago event occurred. Shows exact time of event on mouse hover. Accepts prop in datetime string format.
         `,
-    inline: true,
-  })(() => {
-    return <Finished endTime={demoTimestamp}/>;
-  })
-).add(
-  'Finished with icon',
-  withInfo({
-    inline: true,
-  })(() => {
-    const hasIcon = boolean('showIcon', true);
-    return <Finished showIcon={hasIcon} endTime={demoTimestamp}/>;
-  })
-).add(
-  'No datetime provided',
-  withInfo({
-    inline: true,
-  })(() => {
-    return <Finished/>;
-  })
-);
+      inline: true
+    })(() => {
+      return <Finished endTime={demoTimestamp} showIcon={boolean('showIcon', true)} />;
+    })
+  )
+  .add(
+    'No datetime provided',
+    withInfo({
+      inline: true
+    })(() => {
+      return <Finished />;
+    })
+  );
