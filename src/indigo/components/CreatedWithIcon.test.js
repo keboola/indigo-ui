c7a0dc82144c334ebf94698b222d701139b8affd
janmichek <jan@keboola.com>
2018-12-10 17:53:54 +0100

    adds test and snapshot

---
+++ src/indigo/components/CreatedWithIcon.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import moment from 'moment';
+import { snapshot } from '../../tests';
+import CreatedWithIcon from './CreatedWithIcon';
+
+const time1 = '2018-11-26T15:11:05+0100';
+
+const fakeTimeNow = moment(time1).valueOf();
+global.Date.now = jest.fn(() => fakeTimeNow);
+const timePast = moment(fakeTimeNow - 123456)
+  .format('YYYY-MM-DDTHH:mm')
+  .toString();
+
+describe('<CreatedWithIcon />', () => {
+  it('Relative time', () => {
+    snapshot(<CreatedWithIcon createdTime={timePast} />);
+  });
+
+  it('Absolute time', () => {
+    snapshot(<CreatedWithIcon createdTime={timePast} relative={false} />);
+  });
+});
