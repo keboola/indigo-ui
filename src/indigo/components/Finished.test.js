85e207611a6f5f6edfb709cbf0ede7f78391ee51
janmichek <jan@keboola.com>
2018-12-17 11:55:25 +0100

    fixes timezone for test

--- src/indigo/components/Finished.test.js
+++ src/indigo/components/Finished.test.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 import { snapshot } from '../../tests';
 import Finished from './Finished';
 
-const fakeTimeNow = moment('2018-11-26T16:00:00+0000').valueOf();
+const fakeTimeNow = moment('2018-11-26T16:00:00+0100').valueOf();
 global.Date.now = jest.fn(() => fakeTimeNow);
 const timePast = moment(fakeTimeNow - 123456)
   .format('YYYY-MM-DDTHH:mm:ssZZ')
