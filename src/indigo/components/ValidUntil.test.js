f1aa5fdda0d8631b9e0a27b06a15452ff471872e
janmichek <jan@keboola.com>
2018-12-31 12:12:18 +0100

    fixes test timezone using moment

--- src/indigo/components/ValidUntil.test.js
+++ src/indigo/components/ValidUntil.test.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import moment from 'moment';
 import {snapshot} from '../../tests';
 import ValidUntil from './ValidUntil';
 
-const now = new Date('2018-11-26T15:11:05+0000');
-const timePast = now.getTime() - 50000000;
-const timeFuture = now.getTime() + 50000000;
+const fakeNow = '2018-11-26T15:11:05+0000';
+const fakeNowMs = moment(fakeNow).valueOf();
 
-global.Date.now = jest.fn(() => now);
+global.Date.now = jest.fn(() => fakeNowMs);
+
+const timePast = moment()
+  .subtract(124, 'seconds')
+  .valueOf()
+
+const timeFuture = moment()
+  .add(124, 'seconds')
+  .valueOf()
 
 describe('<ValidUntil />', () => {
-  it('should render `14 hours ago`', () => {
+  it("should render '2 minutes ago'", () => {
     snapshot(<ValidUntil validUntil={timePast} />);
   });
-  it('should render `any time now`', () => {
+  it("should render 'any time now'", () => {
     snapshot(<ValidUntil validUntil={timeFuture} />);
   });
-  it('should render N/A', () => {
+  it("should render 'N/A'", () => {
     snapshot(<ValidUntil />);
   });
 });
