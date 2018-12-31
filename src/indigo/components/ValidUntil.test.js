76c37169d678d60ceddfcd3a52c61c379c375c7d
janmichek <jan@keboola.com>
2018-12-31 12:36:23 +0100

    changes timezone using unix time

--- src/indigo/components/ValidUntil.test.js
+++ src/indigo/components/ValidUntil.test.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {snapshot} from '../../tests';
 import ValidUntil from './ValidUntil';
 
-const timeNow = new Date('2018-11-26T15:11:09+0100').valueOf();
-const timePast = new Date('2018-11-25T15:11:09+0100').valueOf();
-const timeFuture = new Date('2018-11-29T15:11:09+0100').valueOf();
+const timeNow = 1546256044398;
+const timePast = 1546206044398;
+const timeFuture = 1546306044398;
 
 global.Date.now = jest.fn(() => timeNow);
 
 describe('<ValidUntil />', () => {
-  it("should render 'a day ago'", () => {
+  it("should render '14 hours ago'", () => {
     snapshot(<ValidUntil validUntil={timePast} />);
   });
   it("should render 'any time now'", () => {
