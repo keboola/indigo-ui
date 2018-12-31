6d0fb1ef3a5e7545ec1d1916031898b4183b4e3d
janmichek <jan@keboola.com>
2018-12-31 12:01:45 +0100

    fixes test timezone

--- src/indigo/components/ValidUntil.test.js
+++ src/indigo/components/ValidUntil.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {snapshot} from '../../tests';
 import ValidUntil from './ValidUntil';
 
-const now = new Date();
+const now = new Date('2018-11-26T15:11:05+0000');
 const timePast = now.getTime() - 50000000;
 const timeFuture = now.getTime() + 50000000;
 
