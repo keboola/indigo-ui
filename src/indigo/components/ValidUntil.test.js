81e2cfdd711cf3e595b359de1b71ba6b1e523e1e
janmichek <jan@keboola.com>
2018-12-31 14:03:54 +0100

    removes current time mocking

--- src/indigo/components/ValidUntil.test.js
+++ src/indigo/components/ValidUntil.test.js
@@ -2,14 +2,11 @@ import React from 'react';
 import {snapshot} from '../../tests';
 import ValidUntil from './ValidUntil';
 
-const timeNow = 1546256044398;
 const timePast = 1546206044398;
 const timeFuture = 1546306044398;
 
-global.Date.now = jest.fn(() => timeNow);
-
 describe('<ValidUntil />', () => {
-  it("should render '14 hours ago'", () => {
+  it("should render '15 hours ago'", () => {
     snapshot(<ValidUntil validUntil={timePast} />);
   });
   it("should render 'any time now'", () => {
