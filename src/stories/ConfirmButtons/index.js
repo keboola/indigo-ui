b1dfa99e23aeaf513d8b50ce53a0742ee160e976
Jakub Kotek <blaztar@gmail.com>
2019-08-13 11:30:09 +0200

    simplify ConfirmButtons story

--- src/stories/ConfirmButtons/index.js
+++ src/stories/ConfirmButtons/index.js
@@ -1,107 +1,31 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
 import { Button } from 'react-bootstrap';
 import { action } from '@storybook/addon-actions';
 import ConfirmButtons from '../../indigo/components/ConfirmButtons';
 
 storiesOf('ConfirmButtons', module)
-  .add(
-    'Basic Init',
-    withInfo({
-      inline: true,
-    })(() => <ConfirmButtons />)
-  )
-  .add(
-    'Saving',
-    withInfo({
-      inline: true,
-    })(() => <ConfirmButtons isSaving />)
-  )
-  .add(
-    'Disabled',
-    withInfo({
-      text: `
-        Use for validating
-        `,
-      inline: true,
-    })(() => <ConfirmButtons isDisabled />)
-  )
-  .add(
-    'Show Save button',
-    withInfo({
-      inline: true,
-    })(() => <ConfirmButtons showSave={false} />)
-  )
-  .add(
-    'Show Cancel button',
-    withInfo({
-      inline: true,
-    })(() => <ConfirmButtons showCancel={false} />)
-  )
-  .add(
-    'Additional buttons',
-    withInfo({
-      inline: true,
-      text: `
-        Option to add additional class names eg: "pull-right" for alignment
-        `,
-    })(() => (
-      <ConfirmButtons>
-        <Button>Try Query</Button>
-      </ConfirmButtons>
-    ))
-  )
-  .add(
-    'Cancel label',
-    withInfo({
-      inline: true,
-    })(() => <ConfirmButtons cancelLabel="Reset" />)
-  )
-  .add(
-    'Save label',
-    withInfo({
-      inline: true,
-    })(() => <ConfirmButtons saveLabel="Save Configuration" />)
-  )
-  .add(
-    'Save button style',
-    withInfo({
-      inline: true,
-    })(() => <ConfirmButtons saveStyle="primary" />)
-  )
-  .add(
-    'Save button type',
-    withInfo({
-      text: `
-        Uses type="submit" attribute. So it can be wrapped by <form> and submitted by pressing Enter key
-        `,
-      inline: true,
-    })(() => <ConfirmButtons saveButtonType="submit" />)
-  )
-  .add(
-    'Additional class',
-    withInfo({
-      inline: true,
-      text: `
-        Option to add additional class for styling eg: "pull-right" for alignment
-        `,
-    })(() => (
-      <div className="clearfix">
-        <ConfirmButtons className="pull-right" />
-      </div>
-    ))
-  )
-  .add(
-    'Events demo',
-    withInfo({
-      inline: true,
-      text: `
-        onSave & onCancel
-        `,
-    })(() => (
-      <div className="clearfix">
-        <ConfirmButtons onSave={action('event - save')} onCancel={action('event - cancel')} />
-      </div>
-    ))
-  );
+  .add('Basic Init', () => <ConfirmButtons />)
+  .add('Saving', () => <ConfirmButtons isSaving />)
+  .add('Disabled', () => <ConfirmButtons isDisabled />)
+  .add('Show Save button', () => <ConfirmButtons showSave={false} />)
+  .add('Show Cancel button', () => <ConfirmButtons showCancel={false} />)
+  .add('Additional buttons', () => (
+    <ConfirmButtons>
+      <Button>Try Query</Button>
+    </ConfirmButtons>
+  ))
+  .add('Cancel label', () => <ConfirmButtons cancelLabel="Reset" />)
+  .add('Save label', () => <ConfirmButtons saveLabel="Save Configuration" />)
+  .add('Save button style', () => <ConfirmButtons saveStyle="primary" />)
+  .add('Save button type', () => <ConfirmButtons saveButtonType="submit" />)
+  .add('Additional class', () => (
+    <div className="clearfix">
+      <ConfirmButtons className="pull-right" />
+    </div>
+  ))
+  .add('Events demo', () => (
+    <div className="clearfix">
+      <ConfirmButtons onSave={action('event - save')} onCancel={action('event - cancel')} />
+    </div>
+  ));
