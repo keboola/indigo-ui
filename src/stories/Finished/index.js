671a628e32738197da85e36006e2ebc0df4ad9db
Vladimír Kriška <vlado@keboola.com>
2021-11-03 07:57:36 +0100

    Fix typo

--- src/stories/Finished/index.js
+++ src/stories/Finished/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { boolean } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
 import dayjs from '../../indigo/components/date';
 import Finished from '../../indigo/components/Finished';
 
