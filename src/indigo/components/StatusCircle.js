c4a5f4c237c5c59d64e4845415ab3a7f19377abd
Jakub Kotek <blaztar@gmail.com>
2022-06-09 12:13:13 +0200

    warning should have warning style

--- src/indigo/components/StatusCircle.js
+++ src/indigo/components/StatusCircle.js
@@ -4,16 +4,15 @@ import classnames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons/faCircle';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons/faCheckCircle';
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons/faTimesCircle';
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
 
 const statusMap = {
   success: { status: 'success', icon: faCheckCircle },
   error: { status: 'danger', icon: faExclamationCircle },
-  warn: { status: 'danger', icon: faTimesCircle },
-  warning: { status: 'danger', icon: faTimesCircle },
   processing: { status: 'warning', icon: faCircle },
   waiting: { status: 'default', icon: faCircle },
+  warn: { status: 'warning', icon: faExclamationCircle },
+  warning: { status: 'warning', icon: faExclamationCircle },
   cancelled: { status: 'default', icon: faExclamationCircle },
   terminating: { status: 'default', icon: faExclamationCircle },
   terminated: { status: 'default', icon: faExclamationCircle },
