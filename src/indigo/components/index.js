c8f4f866c641a9af6a52b142f44b4055fd2fa33b
Jakub Kotek <blaztar@gmail.com>
2022-09-08 16:55:45 +0200

    remove all custom components from indigo

--- src/indigo/components/index.js
+++
@@ -1,7 +0,0 @@
-import Loader from './Loader';
-import SearchBar from './SearchBar';
-import ExternalLink from './ExternalLink';
-import PanelWithDetails from './PanelWithDetails';
-import ConfirmButtons from './ConfirmButtons';
-
-export { ExternalLink, Loader, PanelWithDetails, SearchBar, ConfirmButtons };
--- src/tests.js
+++
@@ -1,8 +0,0 @@
-import renderer from 'react-test-renderer';
-
-export const matchesSnapshot = (component) => {
-  const tree = renderer
-    .create(component)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-};
